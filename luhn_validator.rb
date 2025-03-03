519f5d4ec70260edefd32092f985aa302604d868
Tedious8 <tadeus.tamus@gmail.com>
2025-03-03 12:23:31 +0800

    feat: Add Validate Checksum

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# This module implements the Luhn algorithm for validating credit card numbers.
+# It checks whether a given card number has a valid checksum.
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -7,5 +11,19 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    parity = nums_a.length % 2
+    check_digit = nums_a.pop
+    sum = (10 - (nums_a.each_with_index.sum { |digit, index| computing_check_digit(digit, index, parity) } % 10)) % 10
+    sum == check_digit
+  end
+
+  private
+
+  def computing_check_digit(digit, index, parity)
+    return digit if index % 2 != parity
+
+    return 2 * digit - 9 if digit > 4
+
+    2 * digit
   end
 end
